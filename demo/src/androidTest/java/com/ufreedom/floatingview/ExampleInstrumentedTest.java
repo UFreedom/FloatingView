97799afc3090aacaa83cb1799b37545bd6090e0e
UFreedom <sunfreedom@sina.cn>
2016-10-26 20:07:52 +0800

    Don't need test

--- demo/src/androidTest/java/com/ufreedom/floatingview/ExampleInstrumentedTest.java
+++ demo/src/androidTest/java/com/ufreedom/floatingview/ExampleInstrumentedTest.java
@@ -1,13 +1,9 @@
 package com.ufreedom.floatingview;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -19,8 +15,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        /*Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ufreedom.floatingview", appContext.getPackageName());
+        assertEquals("com.ufreedom.floatingview", appContext.getPackageName());*/
     }
 }
