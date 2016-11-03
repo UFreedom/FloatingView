fdebb7e48919fe5129fa059b1ad02b039ef10e2c
UFreedom <sunfreedom@sina.cn>
2016-11-03 14:48:11 +0800

    comments for FloatingElement

--- FloatingViewLib/src/main/java/com/ufreedom/floatingview/FloatingElement.java
+++ FloatingViewLib/src/main/java/com/ufreedom/floatingview/FloatingElement.java
@@ -4,23 +4,51 @@ import android.view.View;
 
 import com.ufreedom.floatingview.transition.FloatingTransition;
 
-
 /**
+ * 
+ * Data structure for a floating element.
+ * 
+ * 
  * Author UFreedom
  * Date : 2016 十月 19
  */
 
 public class FloatingElement {
+
     
+    /**
+     * Floating effect, the default is ScaleFloatingTransition
+     */
     public FloatingTransition floatingTransition;
-    
+
+
+    /**
+     * X direction of offset, unit px
+     */
     public int offsetX;
-    
+
+    /**
+     * Y direction of offset, unit px
+     */
     public int offsetY;
-    
+
+
+    /**
+     * The view which you want to float
+     */
     public View targetView;
-    
-    public View anchorView;
-    
+
+
+    /**
+     * The layout resources id of the view which you want to float
+     */
     public int targetViewLayoutResId;
+
+    /**
+     * The view that you want to float above
+     */
+    public View anchorView;
+
+
+
 }
